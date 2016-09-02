c3e0eec445f74bfe7136a3f4c71193d5a00604ef
Bjoern Ellebrecht <bitbucket@be-it-solutions.com>
2016-09-02 21:47:28 +0200

    change order of included files tables (1st order by size, 2nd order bei include order)

--- src/PDW/views/panels/includedFiles.php
+++ src/PDW/views/panels/includedFiles.php
@@ -4,9 +4,7 @@ foreach (get_included_files() as $includeFile) {
 }
 
 $includedFilesSorted = $includedFilesRaw;
-
 arsort($includedFilesSorted);
-
 $count = count($includedFilesRaw);
 
 ?>
@@ -16,22 +14,6 @@ $count = count($includedFilesRaw);
         <h2>Server Info <a class="pdw-panel-close">&times;</a></h2>
     </div>
     <div class="panel-content">
-        <h3 class="collapser">included files (include order) [count: <?php echo $count; ?>]</h3>
-        <table class="pdw-data-table">
-            <thead>
-            <tr>
-                <th>File</th>
-                <th>Size</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach($includedFilesRaw as $filePath => $size):
-                echo '<tr><td>' . $filePath . '</td><td>' . $debugWidget->getHumanReadableSize($size) . '</td></tr>';
-            endforeach;
-            ?>
-            </tbody>
-        </table>
         <h3 class="collapser">included files (size order) [count: <?php echo $count; ?>]</h3>
         <table class="pdw-data-table">
             <thead>
@@ -44,6 +26,22 @@ $count = count($includedFilesRaw);
             <?php
             foreach($includedFilesSorted as $filePath => $size):
                 echo '<tr><td class="align-right"><pre>[' . str_pad(number_format($size, 0,'', '.'), 7, ' ' , STR_PAD_LEFT ) . ' Bytes]</pre></td><td>' . $filePath . '</td>';
+            endforeach;
+            ?>
+            </tbody>
+        </table>
+        <h3 class="collapser closed">included files (include order) [count: <?php echo $count; ?>]</h3>
+        <table class="pdw-data-table">
+            <thead>
+            <tr>
+                <th>File</th>
+                <th>Size</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach($includedFilesRaw as $filePath => $size):
+                echo '<tr><td>' . $filePath . '</td><td>' . $debugWidget->getHumanReadableSize($size) . '</td></tr>';
             endforeach;
             ?>
             </tbody>
